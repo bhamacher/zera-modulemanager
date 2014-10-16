c2ca7c1f33cdb30e9eaba462132c28748aad7dfc
Peter Lohmer <peterlohmer01@gmail.com>
2014-10-16 11:27:08 +0200

    modulemanager.cpp: set path for module plug-ins
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

--- src/modulemanager.cpp
+++ src/modulemanager.cpp
@@ -57,8 +57,8 @@ namespace ZeraModules
      * @todo remove hardcoded path
      */
     //moduleDir = "./zera-modules";
-    //moduleDir = "/usr/lib/zera-modules";
-    moduleDir = "/home/peter/modules";
+    moduleDir = "/usr/lib/zera-modules";
+    //moduleDir = "/home/peter/modules";
 
     foreach (QString fileName, moduleDir.entryList(QDir::Files))
     {
